634df9edd229385ff42c110f275c221e09ecc95c
Alexander Pronskiy <125699859+Sashkey@users.noreply.github.com>
2023-05-06 01:51:11 +0300

    Add files via upload

--- source/main.cpp
+++ source/main.cpp
@@ -1,13 +1,19 @@
 #include "det.h"
 #include "dispenser.h"
 #include "robot.h"
+#include "control.h"
 
 using namespace std;
 using namespace cv;
 
 int main() {
 	shared_ptr<Detector> det(new Detector(0));
+
+	shared_ptr<Control> Control(new Control(det, robot, disp));
+
 	det->set_color_bot_f(34, 65, 255);
 	det->set_color_bot_r(176, 130, 170);
 	det->set_color_target(90, 249, 68);
+
+	Control->run();
 }
